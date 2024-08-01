65026bcef5ac34fc6d1ba486bd0f5442859c8c65
Joel Filho <joelfilho02812@gmail.com>
2024-08-01 12:17:19 -0300

    feat: add user model

---
+++ src/main/java/tech/joelf/e_commerce_api/models/User.java
@@ -0,0 +1,49 @@
+package tech.joelf.e_commerce_api.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    public User() {
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
