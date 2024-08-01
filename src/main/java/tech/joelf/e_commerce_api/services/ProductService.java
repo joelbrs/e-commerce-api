8639c6efc4ae3f8f76dbbfb8e06eacaca6331b5d
Joel Filho <joelfilho02812@gmail.com>
2024-08-01 17:02:51 -0300

    refactor: use modelmapper instead of instanciate product dto out

--- src/main/java/tech/joelf/e_commerce_api/services/ProductService.java
+++ src/main/java/tech/joelf/e_commerce_api/services/ProductService.java
@@ -1,5 +1,6 @@
 package tech.joelf.e_commerce_api.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import tech.joelf.e_commerce_api.dtos.response.ProductDtoOut;
@@ -9,14 +10,15 @@ import tech.joelf.e_commerce_api.repositories.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, ModelMapper modelMapper) {
         this.productRepository = productRepository;
+        this.modelMapper = modelMapper;
     }
 
     public ProductDtoOut getById(Long id) throws Exception {
         var result = productRepository.findById(id).orElseThrow(() -> new Exception("Product not found."));
-        return new ProductDtoOut(result.getId(), result.getName(), result.getDescription(), result.getPrice(),
-                result.getImgUrl());
+        return modelMapper.map(result, ProductDtoOut.class);
     }
 }
