30e56f6ceed76e690a1382a68037f6e427c298a0
Joel Filho <joelfilho02812@gmail.com>
2024-08-01 17:02:17 -0300

    refactor: use class instead of a java records to define dto

--- src/main/java/tech/joelf/e_commerce_api/dtos/response/ProductDtoOut.java
+++ src/main/java/tech/joelf/e_commerce_api/dtos/response/ProductDtoOut.java
@@ -1,4 +1,53 @@
 package tech.joelf.e_commerce_api.dtos.response;
 
-public record ProductDtoOut(Long id, String name, String description, Double price, String imgUrl) {
+public class ProductDtoOut {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDtoOut() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
 }
